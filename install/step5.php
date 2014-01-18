9c8bc8dcf5335ce18a0c631ef74485063bc03c48
NONAKA Kimihiro <nonakap@gmail.com>
2014-01-18 13:37:27 +0900

    Import epgrec UNA 20131229fix2.1.

--- install/step5.php
+++ install/step5.php
@@ -4,17 +4,6 @@ include_once( INSTALL_PATH."/Settings.class.php" );
 
 $settings = Settings::factory();
 
-if( isset( $_GET['script'] ) ){
-	$epg_rec = $_GET['script'];
-	if( !file_exists( INSTALL_PATH.$epg_rec ) ){
-		$alert_msg = '不法侵入者による攻撃を受けました。['.$_SERVER['REMOTE_HOST'].'('.$_SERVER['REMOTE_ADDR'].")]\nSCRIPT::[".$epg_rec.']';
-		reclog( $alert_msg, EPGREC_WARN );
-		file_put_contents( INSTALL_PATH.$settings->spool.'/alert.log', date("Y-m-d H:i:s").' '.$alert_msg."\n", FILE_APPEND );
-		syslog( LOG_WARNING, $alert_msg );
-		exit();
-	}
-}else
-	exit();
 if( isset( $_GET['time'] ) )
 	$rec_time = $_GET['time'];
 else
@@ -22,8 +11,21 @@ else
 
 echo 'EPGの初回受信を行います。'.$rec_time.'分程度後に<a href="'.$settings->install_url.'">epgrecのトップページ</a>を開いてください。';
 
-@exec( INSTALL_PATH.$epg_rec.' >/dev/null 2>&1 &' );
-
+if( isset( $_GET['script'] ) ){
+	$epg_rec = $_GET['script'];
+	switch( $epg_rec ){
+		case '/getepg.php':
+		case '/shepherd.php':
+			@exec( INSTALL_PATH.$epg_rec.' >/dev/null 2>&1 &' );
+			break;
+		default:
+			$alert_msg = '不法侵入者による攻撃を受けました。['.$_SERVER['REMOTE_HOST'].'('.$_SERVER['REMOTE_ADDR'].")]\nSCRIPT::[".$epg_rec.']';
+			reclog( $alert_msg, EPGREC_WARN );
+			file_put_contents( INSTALL_PATH.$settings->spool.'/alert.log', date("Y-m-d H:i:s").' '.$alert_msg."\n", FILE_APPEND );
+			syslog( LOG_WARNING, $alert_msg );
+			break;
+	}
+}
 exit();
 
 ?>
